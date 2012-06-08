c71c02aaa555b6aa85595b1a67a0786c03c0abc4
Sean Chen <sean.chen@leocorn.com>
2012-06-08 09:49:57 -0400

    adding the quick test form to make sure the connection is good.

--- wp-trac-client/admin-settings.php
+++ wp-trac-client/admin-settings.php
@@ -12,6 +12,23 @@ if (isset($_POST['wptc_settings_form_submit']) &&
     echo '<div class="updated"><p><strong>Settings Updated</strong></p></div>';
 }
 
+if (isset($_POST['wptc_quicktest_form_submit']) &&
+    $_POST['wptc_quicktest_form_submit'] == 'Y') {
+
+    $rpcurl = get_blog_option(get_current_blog_id(), 'wptc_rpcurl');
+    $username = get_blog_option(get_current_blog_id(), 'wptc_username');
+    $password = get_blog_option(get_current_blog_id(), 'wptc_password');
+    if ($rpcurl) {
+        require_once 'Zend/XmlRpc/Client.php';
+        $client = new Zend_XmlRpc_Client($rpcurl);
+        $client->getHttpClient()->setAuth($username, $password);
+        $func = $_POST['wptc_function'];
+        //$proxy = $client->getProxy('system');
+        //$methods = $proxy->listMethods();
+        $testResult = $client->call($func, array());
+    }
+   
+}
 ?>
 
 <div class="wrap">
@@ -47,27 +64,39 @@ if (isset($_POST['wptc_settings_form_submit']) &&
     </tbody></table>
   </form>
 
+  <h2>Quick Test</h2>
+  <p>A quick test to make sure the connection is good...</p>
+
+  <form name="wptc_quicktest_form" method="post">
+    <input type="hidden" name="wptc_quicktest_form_submit" value="Y"/>
+    <table class="form-table"><tbody>
+      <tr>
+        <th>Trac XML-RPC Function: </th>
+        <td><input type="text" id="wptc_function" name="wptc_function" 
+                   value="system.listMethods" size="88"/>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row"><input type="submit" name="quickTest" class="button-primary" value="Quick Test" />
+        </th>
+        <td></td>
+      </tr>
+      <tr>
+        <th scope="row">Test Result: </th>
+        <td><pre>
+<?php 
+if ($testResult) { 
+    var_dump($testResult); 
+} 
+?>
+        </pre></td>
+      </tr>
+    </tbody></table>
+  </form>
+
   <p>Some temp Testing:</p>
   <p>Current Blog ID: <?php echo get_current_blog_id() ?></p>
   <p>Current User ID: <?php echo get_current_user_id() ?></p>
   <p>Option blog name: <?php echo get_option('blogname') ?></p>
   <p>include path: <?php echo get_include_path() ?></p>
-
-<?php
-// quick testing...
-$rpcurl = get_blog_option(get_current_blog_id(), 'wptc_rpcurl');
-$username = get_blog_option(get_current_blog_id(), 'wptc_username');
-$password = get_blog_option(get_current_blog_id(), 'wptc_password');
-if ($rpcurl) {
-    require_once 'Zend/XmlRpc/Client.php';
-    $client = new Zend_XmlRpc_Client($rpcurl);
-    $client->getHttpClient()->setAuth($username, $password);
-    $proxy = $client->getProxy('system');
-    $methods = $proxy->listMethods();
-    echo '<pre>';
-    var_dump($methods);
-    echo '</pre>';
-}
-?>
-
 </div>
