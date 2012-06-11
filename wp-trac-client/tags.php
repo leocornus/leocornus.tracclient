6e40d4e01326c2b7f666599afb65f72c1dc5be74
Sean Chen <sean.chen@leocorn.com>
2012-06-11 15:07:08 -0400

    start using multicall for batch requests. it is much faster.

--- wp-trac-client/tags.php
+++ wp-trac-client/tags.php
@@ -13,13 +13,15 @@
  *
  * @return 
  */
-function wptc_get_tickets($milestone, $max=25, $page=1) {
+function wptc_get_tickets($milestone, $version=null, $max=25, $page=1) {
 
     do_action('wptc_get_tickets');
 
+    // get the ticket proxy.
     $proxy = get_wptc_client()->getProxy('ticket');
-    $ids = $proxy->query('milestone='. $milestone . 
-        '&max=' . $max . '&page=' . $page);
+    $queryStr = wptc_build_query($milestone, $version, $max, $page);
+
+    $ids = $proxy->query($queryStr);
     $tickets = array();
     foreach ($ids as $id) {
         $ticket = $proxy->get($id);
@@ -27,4 +29,48 @@ function wptc_get_tickets($milestone, $max=25, $page=1) {
     }
 
     return apply_filters('wptc_get_tickets', $tickets);
+}
+
+/**
+ * the multicall version.
+ */
+function wptc_get_tickets_m($milestone, $version=null, $max=25, $page=1) {
+
+    do_action('wptc_get_tickets_m');
+
+    // get the ticket proxy.
+    $proxy = get_wptc_client()->getProxy('ticket');
+    $queryStr = wptc_build_query($milestone, $version, $max, $page);
+
+    $ids = $proxy->query($queryStr);
+    // preparing the signature.
+    $signatures = array();
+    foreach ($ids as $id) {
+        $sign = array(
+            'methodName' => 'ticket.get',
+            'params' => array($id)
+            );
+        array_push($signatures, $sign);
+    }
+    // get the system proxy.
+    $proxy = get_wptc_client()->getProxy('system');
+    $tickets = $proxy->multicall($signatures);
+
+    return apply_filters('wptc_get_tickets_m', $tickets);
+}
+
+/**
+ * build query based on the given parameters.
+ *
+ */
+function wptc_build_query($milestone, $version=null, $max=25, $page=1) {
+
+    // build the query string.
+    $queryStr = 'milestone=' . $milestone;
+    if ($version != null) {
+        $queryStr = $queryStr . '&version=' . $version;
+    }
+    $queryStr = $queryStr . '&max=' . $max . '&page=' . $page;
+
+    return $queryStr;
 }
