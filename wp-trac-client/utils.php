a62c2cdf6e54b23bd93754b4547780994d5d0570
Sean Chen <sean.chen@leocorn.com>
2013-08-27 10:05:07 -0400

    using filter template_include to apply page templates for page trac trac/ticket and trac/mytickets.

--- wp-trac-client/utils.php
+++ wp-trac-client/utils.php
@@ -15,7 +15,46 @@ function wptc_auto_link_commit_id($subject) {
 
     return $subject;
 }
-
 // filter all wiki content to link commit_id.
 add_filter('wptc_widget_parse_content', 'wptc_auto_link_commit_id', 
            10, 1);
+
+/**
+ *
+ */
+function wptc_apply_page_template($template) {
+
+    if(!is_main_site()) {
+        // current site is not main site.
+        // skip it.
+        return $template;
+    }
+    
+    // using default value to make things easier.
+    // trac homepage.
+    $page_id = get_site_option('wptc_page_trac', "-1");
+    if(($page_id != "-1") && is_page($page_id)) {
+        // apply trac homepage template.
+        $template = WPTC_PLUGIN_PATH . '/templates/page-trac.php';
+        return $template;
+    }
+    // the ticket details page template.
+    $page_id = get_site_option('wptc_page_trac_ticket', "-1");
+    if(($page_id != "-1") && is_page($page_id)) {
+        // apply trac homepage template.
+        $template = WPTC_PLUGIN_PATH . 
+                    '/templates/page-trac-ticket.php';
+        return $template;
+    }
+    // my tickets page.
+    $page_id = get_site_option('wptc_page_trac_mytickets', "-1");
+    if(($page_id != "-1") && is_page($page_id)) {
+        // apply trac homepage template.
+        $template = WPTC_PLUGIN_PATH . 
+                    '/templates/page-trac-mytickets.php';
+        return $template;
+    }
+
+    return $template;
+}
+add_filter('template_include', 'wptc_apply_page_template');
