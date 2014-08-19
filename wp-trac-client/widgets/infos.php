861da3bf50b6b2129bceae2c8b4cc9e860c4e375
Sean Chen <sean.chen@leocorn.com>
2014-08-19 08:57:29 -0400

    show watch / unwatch button for logged in users and watcher summary only for non-logged in users.

--- wp-trac-client/widgets/infos.php
+++ wp-trac-client/widgets/infos.php
@@ -5,16 +5,22 @@
 function wptc_widget_ticket_watching() {
 
     if(!is_user_logged_in()) {
-        // user logged in, return empty string
-        // to leave topnav empty.
-        return "";
+        // user not logged in, only show how many user are watching.
+        $watching_label = <<<EOT
+<span class="watching-label">Watch</span>
+EOT;
+    } else {
+        // logged in user will have a button to watch/unwatch
+        $watching_label = <<<EOT
+<a class="watching-but-unwatch" id="watching-button" href="#">
+Watching</a>
+EOT;
     }
 
     $span = <<<EOT
 <span class="watching">
   <span class="watching-sum">222</span>
-  <a class="watching-but-unwatch" id="watching-button" href="#">
-  Watching</a>
+  {$watching_label}
 </span>
 EOT;
 
