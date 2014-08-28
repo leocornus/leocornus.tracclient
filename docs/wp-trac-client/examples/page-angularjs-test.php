c9a314751a5744453883b5b5f66321f510f0b089
Sean Chen <sean.chen@leocorn.com>
2014-08-28 12:49:51 -0400

    AngularJS todo list app on WordPress page template

--- docs/wp-trac-client/examples/page-angularjs-test.php
+++ docs/wp-trac-client/examples/page-angularjs-test.php
@@ -9,7 +9,38 @@ wp_enqueue_script('wptc-angular-core');
 ?>
 
 <script type="text/javascript">
-jQuery('html').attr('ng-app', '');
+jQuery('html').attr('ng-app', 'todoApp');
+
+jQuery(document).ready(function() {
+// the todo application module.
+angular.module('todoApp', [])
+  .controller('TodoController', ['$scope', function($scope) {
+    $scope.todos = [
+      {text:'learn angular', done:true},
+      {text:'build an angular app', done:false}];
+ 
+    $scope.addTodo = function() {
+      $scope.todos.push({text:$scope.todoText, done:false});
+      $scope.todoText = '';
+    };
+ 
+    $scope.remaining = function() {
+      var count = 0;
+      angular.forEach($scope.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    $scope.archive = function() {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) $scope.todos.push(todo);
+      });
+    };
+  }]);
+});
 </script>
 
 <div>
@@ -18,6 +49,24 @@ jQuery('html').attr('ng-app', '');
          placeholder="Enter a name here">
   <hr>
   <h1>Hello {{yourName}}!</h1>
+</div>
+
+<h2>TODO List</h2>
+
+<div ng-controller="TodoController">
+  <span>{{remaining()}} of {{todos.length}} remaining</span>
+  [ <a href="" ng-click="archive()">archive</a> ]
+  <ul class="unstyled">
+    <li ng-repeat="todo in todos">
+      <input type="checkbox" ng-model="todo.done">
+      <span class="done-{{todo.done}}">{{todo.text}}</span>
+    </li>
+  </ul>
+  <form ng-submit="addTodo()">
+    <input type="text" ng-model="todoText"  size="30"
+           placeholder="add new todo here">
+    <input class="btn-primary" type="submit" value="add">
+  </form>
 </div>
 
 <?php
