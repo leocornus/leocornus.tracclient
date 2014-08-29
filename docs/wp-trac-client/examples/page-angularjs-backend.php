5084a09ead0abc8aeb69a500013c765154e77f6c
Sean Chen <sean.chen@leocorn.com>
2014-08-29 13:08:09 -0400

    create new page template to testing AngularJS incorporate with WordPress backend

---
+++ docs/wp-trac-client/examples/page-angularjs-backend.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Angular Backend Testing
+ * 
+ * Try to wire up AnuglarJS projects tutorial with wp-trac-client backend.
+ */
+get_header();
+?>
+
+<script type="text/javascript">
+jQuery('html').attr('ng-app', 'projects');
+</script>
+
+<?php
+get_footer();
