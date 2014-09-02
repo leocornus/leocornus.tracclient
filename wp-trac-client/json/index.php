74c6eda8433e0b4284a9b2bc14616d4db9b857be
Sean Chen <sean.chen@leocorn.com>
2014-09-02 14:32:15 -0400

    need get ready the JSON APIs for using AngularJS.

---
+++ wp-trac-client/json/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * JSON APIs for wp-trac-client.
+ */
+
+// if this file is called directly, abort!
+if(!defined('WPINC')) {
+    die;
+}
+
+//require_once(WPTC_PLUGIN_PATH . '/json/tickets.php');
