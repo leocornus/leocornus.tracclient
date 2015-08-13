ec2f7494ff1ff0771d686d663d64a0b0c7c15834
Sean Chen <sean.chen@leocorn.com>
2015-08-13 12:05:12 -0400

    introduce the request context class for projects list page.

---
+++ wp-trac-client/lib/Wptc/Context/ProjectsRequestContext.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @namespace
+ */
+namespace Wptc\Context;
+
+use Wptc\Context\RequestContext;
+
+/**
+ * request context for all projects list page.
+ */
+class ProjectsRequestContext extends RequestContext {
+
+    /**
+     * the constructor
+     */
+    public function __construct() {
+
+        $this->init();
+    }
+
+    /**
+     * reset the request context, 
+     * it will be called when user reload page.
+     *
+     * this method will serve normal page load or reload.
+     * it will consider as start point for a page.
+     * 
+     */
+    public function init() {
+
+        // load trac user information.
+        $this->loadTracUser();
+
+        // load filters.
+        $this->loadFilters();
+
+        // load pager states: per_page, page_number, total_items.
+        // this should be the last one to load, as it depends on 
+        // metadata and filters.
+        $this->loadPagerStates();
+    }
+
+    /**
+     * load filter states, which will include:
+     *  - status
+     *  - owner
+     *  - type
+     *  - priority
+     */
+    public function loadFilters() {
+
+        // do nothing for now.
+    }
+
+    /**
+     * load pager states: per_page, page_number, and total_items.
+     */
+    public function loadPagerStates() {
+
+        // === collect pagination information.
+        $per_page = $this->getRequestParam('per_page');
+        // items per page, default is 9
+        // as the projects list page is 3-column rows.
+        if(empty($per_page)) {
+            // set to default per_page to 9,
+            $per_page = 9;
+        }
+        // page number, starts from 0.
+        $page_number = $this->getRequestParam('page_number');
+        if (empty($page_number)) {
+            // set to 0 as the default page number.
+            $page_number = 0;
+        }
+        $this->setState('per_page', $per_page);
+        $this->setState('page_number', $page_number);
+
+        // calculate the total items.
+        // build the query from metedata.
+        $current_query = $this->getState('current_query');
+        $new_query = $this->buildQuery();
+        if(!empty($current_query) && 
+           ($new_query == $current_query)) {
+            // do nothing here as all summary are the same.
+            // the total number should already in cookie.
+        } else {
+            // this is all projects list page.
+            $projects = wptc_get_projects();
+            $this->setState('total_items', count($projects));
+
+            // reset pager number to 0.
+            $this->setState('page_number', 0);
+        }
+    }
+
+    /**
+     * build query based on the metadata and filters. on context..
+     */
+    public function buildQuery() {
+
+        // return empty string for now.
+        return "";
+    }
+}
