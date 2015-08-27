82a0ec9519b50d0394c0fc162bf8b002192e1f76
Sean Chen <sean.chen@leocorn.com>
2015-08-27 15:09:00 -0400

    markup for create new project dialog, in bootstrap modal

--- wp-trac-client/lib/Wptc/View/AllProjectsHome.php
+++ wp-trac-client/lib/Wptc/View/AllProjectsHome.php
@@ -22,12 +22,14 @@ class AllProjectsHome extends ProjectViewBase {
         $content = $this->buildContent();
         // footer.
         $footer = $this->buildFooter();
+        $modals = $this->buildModals();
 
         $the_view = <<<VIEW
 {$header}
 {$navbar}
 {$content}
 {$footer}
+{$modals}
 VIEW;
 
         return $the_view;
@@ -89,7 +91,14 @@ EOT;
         Actions<span class="caret"></span>
       </a>
       <ul class="dropdown-menu">
-        <li><a href="#">Create Project</a></li>
+        <li><a href="" data-toggle="modal" data-target="#newProject">
+          <span class="glyphicon glyphicon-list-alt"></span>
+          Create Project
+        </a></li>
+        <li><a href="#">
+          <span class="glyphicon glyphicon-pencil"></span>
+          Create Ticket
+        </a></li>
         <li role="separator" class="divider"></li>
         <li class="dropdown-header">My...</li>
         <li><a href="#">My Tickets</a></li>
@@ -140,5 +149,101 @@ EOT;
      * build project panel for a project
      */
     public function buildProjectPanel($projectName) {
+    }
+
+    /**
+     * build modals.
+     */
+    public function buildModals() {
+
+        $modals = <<<MODAL
+<div id="newProject" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" 
+                data-dismiss="modal">x</button>
+        <h3 class="modal-title">Create New Project</h3>
+      </div>
+
+      <div class="modal-body">
+<form class="form-horizontal">
+  <fieldset>
+    <div class="form-group">
+      <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+      <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+      <div class="col-lg-10">
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"> Checkbox
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="textArea" class="col-lg-2 control-label">Textarea</label>
+      <div class="col-lg-10">
+        <textarea class="form-control" rows="3" id="textArea"></textarea>
+        <span class="help-block">A longer block of help text that breaks onto a new line and may extend beyond one line.</span>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-lg-2 control-label">Radios</label>
+      <div class="col-lg-10">
+        <div class="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+            Option one is this
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+            Option two can be something else
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="select" class="col-lg-2 control-label">Selects</label>
+      <div class="col-lg-10">
+        <select class="form-control" id="select">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+        <br>
+        <select multiple="" class="form-control">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+    </div>
+  </fieldset>
+</form>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+MODAL;
+
+        return $modals;
     }
 }
