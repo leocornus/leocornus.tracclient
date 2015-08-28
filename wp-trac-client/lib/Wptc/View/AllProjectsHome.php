c4bf9e63166b8f3381e2c1223de3cd78ec1cbc1c
Sean Chen <sean.chen@leocorn.com>
2015-08-28 08:58:08 -0400

    here is the new project form that we will start with.

--- wp-trac-client/lib/Wptc/View/AllProjectsHome.php
+++ wp-trac-client/lib/Wptc/View/AllProjectsHome.php
@@ -86,6 +86,9 @@ EOT;
     <li {$commits_active}><a nohref="{$base_url}?tab=commits">
       <span class="badge">{$total_commits}</span> Commits
     </a></li>
+    <li {$media_active}><a nohref="{$base_url}?tab=media">
+      <span class="badge">{$total_media}</span> Media
+    </a></li>
     <li class="dropdown pull-right">
       <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
         Actions<span class="caret"></span>
@@ -171,64 +174,26 @@ EOT;
 <form class="form-horizontal">
   <fieldset>
     <div class="form-group">
-      <label for="inputEmail" class="col-lg-2 control-label">Email</label>
-      <div class="col-lg-10">
-        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+      <label for="inputName" class="col-lg-4 control-label">Project Name</label>
+      <div class="col-lg-8">
+        <input type="text" class="form-control" id="inputName" placeholder="set name for your project">
+        <span class="help-block">Using only letter and number for project name. NO whitespaces and special chareters</span>
       </div>
     </div>
     <div class="form-group">
-      <label for="inputPassword" class="col-lg-2 control-label">Password</label>
-      <div class="col-lg-10">
-        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-        <div class="checkbox">
-          <label>
-            <input type="checkbox"> Checkbox
-          </label>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="textArea" class="col-lg-2 control-label">Textarea</label>
-      <div class="col-lg-10">
-        <textarea class="form-control" rows="3" id="textArea"></textarea>
+      <label for="projectDescription" class="col-lg-4 control-label">Project Description</label>
+      <div class="col-lg-8">
+        <textarea class="form-control" rows="3" id="projectDescription"></textarea>
         <span class="help-block">A longer block of help text that breaks onto a new line and may extend beyond one line.</span>
       </div>
     </div>
     <div class="form-group">
-      <label class="col-lg-2 control-label">Radios</label>
-      <div class="col-lg-10">
-        <div class="radio">
-          <label>
-            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-            Option one is this
-          </label>
-        </div>
-        <div class="radio">
-          <label>
-            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-            Option two can be something else
-          </label>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="select" class="col-lg-2 control-label">Selects</label>
-      <div class="col-lg-10">
-        <select class="form-control" id="select">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-        <br>
-        <select multiple="" class="form-control">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
+      <label for="inputOwners" class="col-lg-4 control-label">
+        Project Owners
+      </label>
+      <div class="col-lg-8">
+        <input type="text" class="form-control" id="inputOwners" placeholder="set project owners">
+        <span class="help-block">Multiple owners are separated by ','</span>
       </div>
     </div>
   </fieldset>
