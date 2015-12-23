9ff263897864c374c6f2efffbfa09c05773bd7be
Sean Chen <sean.chen@leocorn.com>
2015-12-23 10:01:32 -0500

    move the utility functions for kanban view to the base class.

--- wp-trac-client/lib/Wptc/View/ProjectViewBase.php
+++ wp-trac-client/lib/Wptc/View/ProjectViewBase.php
@@ -303,4 +303,103 @@ ORDER;
 
         return $order;
     }
+
+    /**
+     * build the 3 column kanban page.
+     */
+    public function buildKanbanContent() {
+
+        $todo_panel = 
+            $this->buildKanbanPanel('TODO', 'danger',
+                   'Tickets with status <span class="label label-danger">new</span> and <span class="label label-danger">reopened</span>');
+        $doing_panel = 
+            $this->buildKanbanPanel('DOING', 'warning',
+                   'Tickets with status <span class="label label-primary">assigned</span> and <span class="label label-primary">accepted</span>');
+        $done_panel = 
+            $this->buildKanbanPanel('DONE', 'success',
+                   'Tickets with status <span class="label label-success">closed</span>');
+
+        $content = <<<EOT
+<div id="project-content" class="container-fluid">
+  <!--h2 class="bg-info text-center">Loading ...</h3 -->
+  <div class="row">
+    <div class="col-sm-4" id="col-todo">
+      {$todo_panel}
+    </div>
+
+    <div class="col-sm-4" id="col-doing">
+      {$doing_panel}
+    </div>
+
+    <div class="col-sm-4" id="col-done">
+      <!-- list of open tickets, sort by priority -->
+      <!-- backlog, open tickets.. -->
+      {$done_panel}
+    </div>
+  </div>
+</div> <!-- project-content -->
+EOT;
+
+        return $content;
+    }
+
+    /**
+     * build the kanban panel,
+     */
+    public function buildKanbanPanel($kanban_name, $panel_color,
+                                     $panel_desc=null) {
+
+        if($panel_desc == null) {
+            $panel_desc = "Tickets which have not been assigned to any .";
+        }
+
+        //$status_checkbox = $this->buildCheckboxStatus();
+        $status_checkbox = <<<CHECK
+<a href="#" class="btn btn-xs btn-success" 
+   id="kanban-{$kanban_name}-status-closed"
+   data-toggle="tooltip" title="Show/Hide Closed Tickets"
+>
+  <span class="glyphicon glyphicon-unchecked"></span> closed
+</a>
+CHECK;
+        // <i class="fa fa-chevron-left fa-border"></i><i class="fa fa-chevron-right fa-border"></i>
+        $panel = <<<PANEL
+      <div class="panel panel-{$panel_color}" 
+           id="kanban-{$kanban_name}"
+      >
+        <div class="panel-heading">
+          <span class="panel-title">
+            <i class="fa fa-th-large fa-lg"></i> {$kanban_name}
+            <span class="pull-right" id="kanban-{$kanban_name}-summary">
+            <small>
+              <span class="btn-group">
+                <span id="start">1</span>-<span id="end"></span> 
+                of <strong><span id="total"></span></strong>
+              </span>
+              <div class="btn-group">
+                <a class="btn btn-xs btn-{$panel_color}" 
+                   id="previous">
+                  <i class="fa fa-chevron-left"></i>
+                </a>
+                <a class="btn btn-xs btn-{$panel_color}" 
+                   id="next">
+                  <i class="fa fa-chevron-right"></i>
+                </a> 
+              </div>
+            </small>
+            </span>
+          </span>
+        </div>
+        <div class="panel-body bg-info" 
+             id="kanban-{$kanban_name}-body"
+        >
+          {$panel_desc}
+        </div> <!-- panel-body -->
+<div class="list-group" id="kanban-{$kanban_name}-list-group">
+</div>
+      </div>
+PANEL;
+
+        return $panel;
+    }
 }
