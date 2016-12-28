1d400c6c89f7b5daaa22c6533bbb6bd5854a5936
Sean Chen <sean.chen@leocorn.com>
2016-12-28 11:13:58 -0500

    one of the ways to use the dashicon for toolbar.

--- wp-trac-client/toolbar/custom-toolbar.php
+++ wp-trac-client/toolbar/custom-toolbar.php
@@ -7,28 +7,25 @@ function add_projects_toolbar_menu() {
 
     global $wp_admin_bar;
 
+    // css way to use dashicons.
+    $icon = '<span class="ab-icon"></span>';
+    // HTML way to use dashicons.
+    //$icon = '<span class="dashicons dashicons-portfolio"></span>';
+
+    // add Projects menu for logged in users.
+    $wp_admin_bar->add_menu( array(
+        'id' => 'projects',
+        'parent' => 'top-secondary',
+        'title' => $icon . 'Projects',
+        'href' => '/projects'
+    ));
+    add_sub_menu( 'Create Ticket', '/trac/ticket', 'projects');
+
     if ( is_user_logged_in() ) {
     
-        // add Projects menu for logged in users.
-        $wp_admin_bar->add_menu( array(
-            'id' => 'projects',
-            'parent' => 'top-secondary',
-            'title' => '<span class="ab-icon"></span>Projects',
-            'href' => '/projects'
-        ));
-        add_sub_menu( 'Create Ticket', '/trac/ticket', 'projects');
-        add_sub_menu( 'My Tickets', '/projects?tab=mytickets', 'projects');
+        add_sub_menu( 'My Tickets', '/projects?tab=mytickets', 
+                      'projects');
         add_sub_menu( 'GitWeb', '/gitweb', 'projects');
-    } else {
-
-        // add Projects menu for anonymous users.
-        $wp_admin_bar->add_menu( array(
-            'id' => 'projects',
-            'parent' => 'top-secondary',
-            'title' => '<span class="ab-icon"></span>Projects',
-            'href' => '/projects'
-        ));
-        add_sub_menu( 'Create Ticket', '/trac/ticket', 'projects');
     }
 }
 // hook to action 'admin_bar_menu'.
--- wp-trac-client/toolbar/index.php
+++ wp-trac-client/toolbar/index.php
@@ -5,4 +5,17 @@ if(!defined('WPINC')) {
     die;
 }
 
+//add_action( 'wp_enqueue_scripts', 'load_dashicons_style' );
+/**
+ * Enqueue Dashicons style for frontend use
+ */
+//function load_dashicons_style() {
+//        wp_enqueue_style( 'dashicons' );
+//}
+
+wp_register_style( 'custom-toolbar', 
+                   plugins_url('custom-toolbar.css', __FILE__) );
+wp_enqueue_style( 'custom-toolbar' );
+
 require_once(WPTC_PLUGIN_PATH . '/toolbar/custom-toolbar.php');
+
